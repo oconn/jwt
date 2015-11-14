342830f8021d6ee76b82c006c3f6d4e6019634b9
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2015-11-13 21:31:51 -0500

    Initial commit
    - Add jws lib
    - create wrapper for decode, sign, and verify.

---
+++ index.js
@@ -0,0 +1,9 @@
+var sign = require('./lib/sign');
+var verify = require('./lib/verify');
+var decode = require('./lib/decode');
+
+module.exports = {
+    decode: require('./lib/decode'),
+    sign: require('./lib/sign'),
+    verify: require('./lib/verify')
+}
