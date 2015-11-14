de1d050364577256862107d7e8bb4dddf545fdb9
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2015-11-14 15:44:41 -0500

    add helper methods

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var sign = require('./lib/sign');
-var verify = require('./lib/verify');
-var decode = require('./lib/decode');
-
 module.exports = {
     decode: require('./lib/decode'),
     sign: require('./lib/sign'),
-    verify: require('./lib/verify')
+    verify: require('./lib/verify'),
+    helpers: require('./lib/helpers')
 }
